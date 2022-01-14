25a7fc87c2b33c4a3fdfd6d0b2dde917faf83971
rilina1986 <76787235+rilina1986@users.noreply.github.com>
2022-01-14 22:35:46 +0200

    created setup.py for possibility to reuse repo with pip

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup
 
+setup(
+    name='playing_with_selenium',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='benas1986',
+    author_email='',
+    description=''
+)
